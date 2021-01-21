f66a7bfc01282891f357a09d1b911d7bb470f163
Eric Larson <ewlarson@gmail.com>
2021-01-21 15:10:48 -0600

    LAG3945 - Resolve journal title fielded queries
    Fixes LAG3945
    
    Co-Authored-By: Jamie Little <jamie@jamielittle.org>
    Co-Authored-By: fackrellj <75277580+fackrellj@users.noreply.github.com>

--- app/search_builder_filters/journal_title_apply_limit.rb
+++ app/search_builder_filters/journal_title_apply_limit.rb
@@ -1,19 +1,19 @@
 module JournalTitleApplyLimit
 
   def self.included(klass)
-    i = klass.default_processor_chain << :journal_title_apply_limit    
+    i = klass.default_processor_chain << :journal_title_apply_limit
   end
 
   # Journal Title field is selected, insert a facet limit for
   # just Journal, that's what it means. If we change
   # the value from "Journal/Newspaper", we'd have to change
-  # below too. 
+  # below too.
   def journal_title_apply_limit(solr_params)
 
-    if blacklight_params["search_field"] == "journal_title"
+    if blacklight_params["search_field"] == "journal"
       solr_params[:fq] ||= []
       solr_params[:fq] << "{!raw f=format}Journal/Newspaper"
-    end        
+    end
 
     return solr_params
   end
