45cfa7c6ce52ba90c9109046f45ef2e76566e250
Jon Fackrell <jon.fackrell@jhu.edu>
2021-01-22 13:31:25 -0700

    Fixed error when coming from advanced search

--- app/assets/javascripts/catalog.js
+++ app/assets/javascripts/catalog.js
@@ -2,17 +2,17 @@
 // All this logic will automatically be available in application.js.
 
 Blacklight.onLoad(function() {
-    // Update selected field on page load if target (search_field) is present in URL
-    selectedSerachField = $('.search-query-form.catalog-search input[name="search_field"]').val() || '';
-    if(selectedSerachField.length > 0 && selectedSerachField != 'any_fields'){
-        search_field_label = $('.search-field-form-selector .dropdown-menu .dropdown-item[data-target="' +  $('.search-query-form.catalog-search input[name="search_field"]').val() + '"]')[0].innerHTML;
+    // Update selected field if target (search_field) is present in URL
+    var selectedSearchField = $('.search-query-form.catalog-search input[name="search_field"]').val() || '';
+    if(selectedSearchField.length > 0 && (selectedSearchField !== 'all_fields' && selectedSearchField !== 'advanced')){
+        var search_field_label = $('.search-field-form-selector .dropdown-menu .dropdown-item[data-target="' +  $('.search-query-form.catalog-search input[name="search_field"]').val() + '"]')[0].innerHTML;
         $('#targetDropdownMenuButton').html(search_field_label);
     }
 
     // Update search field for catalog search
     $('.search-field-form-selector .dropdown-menu > .dropdown-item').on('click', function(e) {
-        search_field_label = $(this)[0].innerHTML;
-        search_field = $(this).data('target');
+        var search_field_label = $(this)[0].innerHTML;
+        var search_field = $(this).data('target');
 
         // Relabel form selector button and set a form data attr
         $('#targetDropdownMenuButton').html(search_field_label);
