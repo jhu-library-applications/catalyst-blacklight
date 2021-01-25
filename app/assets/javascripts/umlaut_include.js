357ea3a16421064dfb57c02993b22918a84c21ce
fackrellj <75277580+fackrellj@users.noreply.github.com>
2021-01-25 13:43:03 -0700

    Show 856 links (#29)
    * If Findit returns no results, but there is an 856 link, then don't hide it.
    
    * Updated logic and removed debug code

--- app/assets/javascripts/umlaut_include.js
+++ app/assets/javascripts/umlaut_include.js
@@ -44,7 +44,11 @@ Blacklight.onLoad(function(){
         $(this).text(text);
       });
     } else {
-      $(html).closest(".links").find(".marc856").find('.marc856*:not(:contains("Finding aid")):not(:contains("Finding Aid")):not(:contains("Complete inventory")):not(:contains("Collection guide available"))').closest('li.marc856').hide();
+      if( $(html).closest(".links").find('.umlaut_section_content > p').hasClass('umlaut-unavailable') && $(html).closest(".links").find('.marc856url a').length > 0 ){
+        $('p.umlaut-unavailable').hide();
+      } else{
+        $(html).closest(".links").find(".marc856").find('.marc856*:not(:contains("Finding aid")):not(:contains("Finding Aid")):not(:contains("Complete inventory")):not(:contains("Collection guide available"))').closest('li.marc856').hide();
+      }
       if ($(html).closest(".links").find(".marc856*:contains('Finding aid'),.marc856*:contains('Finding Aid'),.marc856*:contains('Complete inventory')").length > 0) {
         $('p.umlaut-unavailable').hide();
         // Update label before finding aid url
@@ -55,7 +59,7 @@ Blacklight.onLoad(function(){
     }
 
     if ($("#hathi-etas").attr("hathi-present").trim() == "true"){
-        $('p.umlaut-unavailable').hide();
+      $('p.umlaut-unavailable').hide();
     }
   }
 
