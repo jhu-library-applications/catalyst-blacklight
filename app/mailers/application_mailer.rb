1b3e4ab9c12539ba48f18c69ab33ac3bfac5fdb4
fackrellj <75277580+fackrellj@users.noreply.github.com>
2021-02-08 15:37:06 -0700

    Changed default email address from catalog@jhu.edu to no-reply@jhu.edu (#51)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '"Johns Hopkins Libraries" <catalog@jhu.edu>'
+  default from: '"Johns Hopkins Libraries" <no-reply@jhu.edu>'
   layout 'mailer'
 end
 
