d12c0525d681315c59d9795e61e7c9566aea5b0a
Jamie Little <jamie@jamielittle.org>
2021-03-18 15:13:17 -0500

    LAG-3997 Catalyst stored searches
    This adds a nightly task using `whenever` to
    clear out saved searches.The cron tab
    entries are installed on the server when the
    application is deployed with `capistrano`.
    
    It looks like tasks and the capistrano/whenever
    setup existed before, but weren't migrated when
    the move to `ansible` happened.
    
    I removed the existing tasks for cleaning out searches
    and users. As we accumulate users we will need to
    reinstate a similar task, but we will need to be careful
    because we won't be able to use `TRUNCATE` like we are
    with searches.

---
+++ config/schedule.rb
@@ -0,0 +1,15 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+
+every :day, at: '2:00am' do
+   rake 'nightly_cleanup'
+end
+
+# Learn more: http://github.com/javan/whenever
