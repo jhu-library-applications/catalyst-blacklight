8ed2e6083b2f405a69153a99d8fcd2d8ca6d70dd
fackrellj <75277580+fackrellj@users.noreply.github.com>
2021-04-02 15:01:00 -0600

    Updates to My Account links and Catalyst Service Name (#101)
    * Modified title and started re-arranging user account links
    
    * Added user account menu
    
    * Updated dropdown id
    
    * Reordered links and added right margin
    
    * Updated tests for account menu changes
    
    * Updated tests
    
    * added local rake command for running ci tests that uses solr_wrapper
    
    * Removed rake command. Will commit separately.
    
    * Removed the Dev.md file that was already deleted from main.
    
    * Updated active link style and account link locations
    
    * Fixed padding issue in Firefox and Safari. Removed button styling for Safari
    
    * Fixed horizontal scrolling in search results by removing overflow:auto on the main-container
    
    * removed random id name I accidentally pasted in
    
    * fixed some of the header layout issues
    
    * Library name was changed in StackMap thus needed updated here
    
    * Revert "Library name was changed in StackMap thus needed updated here"
    
    This reverts commit c172e28433fb66628a332bd28dca7ae2f3d774b2.

--- test/system/homepage_test.rb
+++ test/system/homepage_test.rb
@@ -18,13 +18,14 @@ class HomepageTest < ApplicationSystemTestCase
       assert page.has_link?("Arthur Friedheim Library")
       assert page.has_link?("APL Library")
     end
+    within("ul.account-dropdown") do
+      assert page.has_link?("Bookmarks")
+      assert page.has_link?("Login")
+    end
     within("div#jhu-app-nav") do
       assert page.has_selector?("img")
       assert page.has_link?("Catalyst")
       assert page.has_link?("Other Resources")
-      assert page.has_link?("Bookmarks")
-      assert page.has_link?("Account")
-      assert page.has_link?("Login")
     end
     assert page.has_selector?("div.search-container")
     within("div.search-container") do
@@ -32,7 +33,6 @@ class HomepageTest < ApplicationSystemTestCase
       within("div.search-navbar") do
         assert page.has_text?("Catalog")
         assert page.has_link?("Articles")
-        assert page.has_link?("Reserves")
       end
       assert page.has_selector?("form.catalog-search")
       within("form.catalog-search") do
