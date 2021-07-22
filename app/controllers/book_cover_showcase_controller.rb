91bc0c69adf8510376174585ee846db5ee042509
Jon Fackrell <jon.fackrell@jhu.edu>
2021-07-22 09:49:00 -0600

    Added isbns to json, reject items without isbns, check for syndetics parameter to skip image lookup

--- app/controllers/book_cover_showcase_controller.rb
+++ app/controllers/book_cover_showcase_controller.rb
@@ -16,10 +16,11 @@ class BookCoverShowcaseController < CatalogController
         render json: {'bookcovers': @response.docs.map{|doc|
           {
             title: doc['title_display'],
-            imageURL: book_cover(doc['isbn_t']),
-            catalystURL: 'https://catalyst.library.jhu.edu/catalog/' + doc['id'].to_s
+            imageURL: params.has_key?('syndetics') ? '' : book_cover(doc['isbn_t']),
+            catalystURL: 'https://catalyst.library.jhu.edu/catalog/' + doc['id'].to_s,
+            isbns: doc['isbn_t']
           }
-        }.reject{|doc| doc[:imageURL].nil? }}.as_json, callback: params['callback']
+        }.reject{|doc| doc[:imageURL].nil? || doc[:isbns].nil? }}.as_json, callback: params['callback']
       end
     end
   end
