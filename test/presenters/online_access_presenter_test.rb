9a1c8a8e9c26a1c2d9a83c683615401699390c0e
Jamie Little <jamie@jamielittle.org>
2022-01-27 09:22:56 -0600

    HELP-26545 Add 1944-2000 coverage of Le Monde to Catalyst record (#204)
    This fixes a problem where multiple coverage statements are not
    shown.

--- test/presenters/online_access_presenter_test.rb
+++ test/presenters/online_access_presenter_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 # Test the presenter directly, also tested indirectly through system tests
 class OnlineAccessPresenterTest < ActiveSupport::TestCase
   def setup
-    @presenter = OnlineAccessPresenter.new(urls: []).overflow
+    @presenter = OnlineAccessPresenter.new(targets: []).overflow
   end
 
   test 'supports external_links' do
-    assert @presenter[:show_urls].kind_of? Array
+    assert @presenter[:show_targets].kind_of? Array
   end
 end
