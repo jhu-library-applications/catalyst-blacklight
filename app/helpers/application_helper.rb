5a040da660287c452b7cebe5118781d9c6e47bd4
Jamie Little <jamie@jamielittle.org>
2022-02-09 15:45:12 -0600

    Hotfix/2.7.9 (#208)
    * LAG-4727 Test back button behavior on Catalyst detail record
    
    This reduces the threshold that triggers the loading of SFX links.
    
    hotfix/2.7.7
    
    * LAG-4727 Test back button behavior on Catalyst detail record
    
    This adds the `tmp/cache` folder to capistrano's shared folders
    this will avoid creating a new cache when deploying.
    
    * LAG-4727 Test back button behavior on Catalyst detail record
    
    This adds a helper method used for URI decoding the links
    from SFX. HeinOnline links were not working correctly without
    this change.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
+require 'uri'
 #require_dependency 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-
-
-
+  def url_decode(url)
+    URI.decode(url)
+  end
+ 
   # Over-ride Umlaut default
   def application_name
     "JH Libraries"
